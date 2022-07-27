876080a3ff5f78d8c92f43b10908fd0079ba7e65
AR31313 <gurlgotgame@gmail.com>
2022-07-27 07:07:09 -0700

    back to working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './Components/Nav/Nav'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
-  var hello = "tacocat"
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {hello}
+          Learn React
         </a>
       </header>
     </div>
